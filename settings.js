8df0b44f5917c1d59978559202d4d5dc70671cb3
LEE <networkserver06@gmail.com>
2026-02-23 18:27:42 +0300

    Update settings.js

--- settings.js
+++ settings.js
@@ -3,7 +3,7 @@ const settings = {
   author: '‎',
   botName: "Knight Bot",
   botOwner: 'Lee Tech', // Your name
-  ownerNumber: '254791656621', //Set your number here without + symbol, just add country code & number without any space
+  ownerNumber: '', //Set your number here without + symbol, just add country code & number without any space
   giphyApiKey: 'qnl7ssQChTdPjsKta2Ax2LMaGXz303tq',
   commandMode: "public",
   maxStoreMessages: 20, 
