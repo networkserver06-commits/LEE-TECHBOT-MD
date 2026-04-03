f1ea48947951dd9490692647f28b8cfca25959f6
LEE <networkserver06@gmail.com>
2026-04-03 15:10:24 +0000

    done

--- index.js
+++ index.js
@@ -219,7 +219,7 @@ async function startXeonBotInc() {
         if (!!global.phoneNumber) {
             phoneNumber = global.phoneNumber
         } else {
-            phoneNumber = await question(chalk.bgBlack(chalk.greenBright(`Please type your WhatsApp number 😍\nFormat: 6281376552730 (without + or spaces) : `)))
+            phoneNumber = await question(chalk.bgBlack(chalk.greenBright(`Please type your WhatsApp number 😍\nFormat: 2547123456779 (without + or spaces) : `)))
         }
 
         // Clean the phone number - remove any non-digit characters
